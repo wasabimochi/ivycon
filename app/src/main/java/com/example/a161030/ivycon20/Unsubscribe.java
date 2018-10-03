329caf131641d584a5abc5249e2cc9f84b6303b0
fujibro <huzita.mizuki@gmail.com>
2018-10-03 09:47:30 +0900

    no message

--- app/src/main/java/com/example/a161030/ivycon20/Unsubscribe.java
+++ app/src/main/java/com/example/a161030/ivycon20/Unsubscribe.java
@@ -1,17 +1,21 @@
 package com.example.a161030.ivycon20;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
 
-public class Unsubscribe extends AppCompatActivity{
+import static android.app.PendingIntent.getActivity;
+
+public class Unsubscribe extends AppCompatActivity {
 
     private StudentTimeline studentTimeline;
 
@@ -21,23 +25,32 @@ public class Unsubscribe extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.unsubscribe);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("今からアプリを起動してもいいですか？")
-                .setPositiveButton("起動", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                    // ボタンをクリックしたときの動作
+                builder.setTitle("確認");
+                builder.setMessage("アカウントを削除しますか");
+                builder.setPositiveButton("削除する", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // OK button pressed
                         //ユーザーの情報を削除
                         studentTimeline.user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
                                     Log.d(TAG, "アカウント消えた");
+                                    //ログイン画面へ
+                                    Intent intent = new Intent(getApplication(), TeacherDepartment.class);
+                                    startActivity(intent);  //画面遷移
                                 }
                             }
                         });
-
+                        Toast.makeText(Unsubscribe.this, "アカウントを削除しました。", Toast.LENGTH_SHORT).show();
                     }
-                });
-        builder.show();
+                })
+                .setNegativeButton("削除しない", null)
+                .show();
         }
-    }
+
+}
+
